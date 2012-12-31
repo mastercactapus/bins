246716aeb1cae3844947fb3dde3c34ed20e37783
Nathan Caza <nathan@nathancaza.com>
2012-12-31 02:36:54 -0600

    added test code for development

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,9 +6,27 @@ window.bins = {
   Routers: {},
   init: function() {
     console.log('Hello from Backbone!');
+    test();
+    console.log('made that shit');
   }
 };
 
 $(document).ready(function(){
   bins.init();
 });
+
+
+function test(){
+  var drawers = new bins.Collections.cabinetDrawers;
+  drawers.add(_.map(_.range(25),function(i){
+    new bins.Models.Drawer
+  }));
+  var cab = new bins.Models.Cabinet({"drawers": drawers});
+
+  var cabV = new bins.Views.cabinetView({
+    el: $("#test"),
+    model: cab
+  });
+
+  cabV.render();
+}
