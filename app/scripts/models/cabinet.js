8b3836b3c496d780b160e776020e97ee62e49680
Nathan Caza <nathan@nathancaza.com>
2012-12-31 02:37:07 -0600

    added layout code for cabinet

--- app/scripts/models/cabinet.js
+++ app/scripts/models/cabinet.js
@@ -8,8 +8,38 @@
 bins.Models.Cabinet = Backbone.Model.extend({
   defaults: function(){
     return {
-      drawers: new bins.cabinetDrawers,
-      rows: 1
+      id:0,
+      drawers: new bins.Collections.cabinetDrawers,
+      cols: 5,
+      rows:4,
+      description: "",
+      selected:null
+    }
+  },
+
+  /**
+   * Generates a map of the drawer layout for templating
+   * @return {Object} An object representing the current drawer layout
+   */
+  layout: function(){
+    var self=this;
+    var rows = _.map( _.range(self.get('rows')), function(y){
+      var cols = _.map( _.range(self.get('cols')), function(x){
+        var drawer_id = (y*self.get('cols')) + +x;
+        return {
+          width: (1/self.get('cols') * 100) + "%",
+          id: drawer_id,
+          selected: (self.get('selected') == drawer_id)
+        }
+      });
+      return {
+        height: (1/self.get('rows') * 100) + "%",
+        cols: cols
+      }
+    });
+    return {
+      cabinet: self.id,
+      rows: rows
     }
   }
 });
