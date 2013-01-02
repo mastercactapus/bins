15b39bd25e3e014bc2cdd1596509e7b9d4fb92e9
Nathan Caza <mastercactapus@gmail.com>
2013-01-01 18:04:28 -0600

    updated cabinet model

--- app/scripts/models/cabinet.js
+++ app/scripts/models/cabinet.js
@@ -8,38 +8,22 @@
 bins.Models.Cabinet = Backbone.Model.extend({
   defaults: function(){
     return {
-      id:0,
       drawers: new bins.Collections.cabinetDrawers,
-      cols: 5,
-      rows:4,
-      description: "",
-      selected:null
+      width: 5,
+      label: null,
+      description: null
     }
   },
 
-  /**
-   * Generates a map of the drawer layout for templating
-   * @return {Object} An object representing the current drawer layout
-   */
-  layout: function(){
-    var self=this;
-    var rows = _.map( _.range(self.get('rows')), function(y){
-      var cols = _.map( _.range(self.get('cols')), function(x){
-        var drawer_id = (y*self.get('cols')) + +x;
-        return {
-          width: (1/self.get('cols') * 100) + "%",
-          id: drawer_id,
-          selected: (self.get('selected') == drawer_id)
-        }
-      });
-      return {
-        height: (1/self.get('rows') * 100) + "%",
-        cols: cols
-      }
-    });
-    return {
-      cabinet: self.id,
-      rows: rows
-    }
+  clearSelection: function(){
+    this.set('selected',{});
+  },
+
+  selectOne: function(id){
+    this.clearSelection();
+    this.selectId(id);
+  },
+  selectId: function(id){
+    this.get('selected')[id] = "selected";
   }
 });
