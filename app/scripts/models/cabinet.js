d4f44f5d7d5301d1ee13debb0d27b30d3b82f6b4
Nathan Caza <mastercactapus@gmail.com>
2013-01-03 13:15:57 -0600

    simplified cabinet

--- app/scripts/models/cabinet.js
+++ app/scripts/models/cabinet.js
@@ -10,36 +10,9 @@ bins.Models.Cabinet = Backbone.Model.extend({
     return {
       id:0,
       drawers: new bins.Collections.cabinetDrawers,
-      cols: 5,
-      rows:4,
-      description: "",
-      selected:null
-    }
-  },
-
-  /**
-   * Generates a map of the drawer layout for templating
-   * @return {Object} An object representing the current drawer layout
-   */
-  layout: function(){
-    var self=this;
-    var rows = _.map( _.range(self.get('rows')), function(y){
-      var cols = _.map( _.range(self.get('cols')), function(x){
-        var drawer_id = (y*self.get('cols')) + +x;
-        return {
-          width: (1/self.get('cols') * 100) + "%",
-          id: drawer_id,
-          selected: (self.get('selected') == drawer_id)
-        }
-      });
-      return {
-        height: (1/self.get('rows') * 100) + "%",
-        cols: cols
-      }
-    });
-    return {
-      cabinet: self.id,
-      rows: rows
+      width: 5,
+      label: null,
+      selected: null
     }
   }
 });
