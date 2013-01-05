99feebdf5e4eab13c69bed2888e52f8356016894
Nathan Caza <mastercactapus@gmail.com>
2013-01-05 01:08:26 -0600

    Rewrote build file, updated index and main.js accordingly Also added dev dependencies
    grunt server -- server watch mode
    grunt        -- default build into temp

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,216 +1,66 @@
-module.exports = function( grunt ) {
-  'use strict';
-  //
-  // Grunt configuration:
-  //
-  // https://github.com/cowboy/grunt/blob/master/docs/getting_started.md
-  //
-  //
-  grunt.loadNpmTasks('grunt-dustjs');
-  grunt.initConfig({
-      // load dustjs task
+module.exports = function(grunt) {
 
-    // Project configuration
-    // ---------------------
-    
-    // compile dust templates
+  //additional task imports
+  grunt.loadNpmTasks('grunt-dustjs');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.initConfig({
     dustjs: {
       compile: {
         files: {
-          "temp/scripts/dust-templates.js": "app/scripts/**/*.dust"
+          "temp/scripts/dust-templates.js": ["app/scripts/**/*.dust"]
         }
       }
     },
-
-    //concat backbone js files
+    clean: {
+      temp: ['temp']
+    },
     concat: {
       dist: {
-        src: ["app/scripts/models/*.js","app/scripts/collections/*.js","app/scripts/views/*.js","app/scripts/routes/*.js"],
-        dest: 'temp/scripts/backbone-definitions.js'
+        src: ["app/scripts/init.js","app/scripts/vendor/jquery.min.js","app/scripts/vendor/lodash.min.js","app/scripts/vendor/dust-core.min.js","app/scripts/vendor/dust-helpers.min.js","app/scripts/vendor/backbone-min.js", "temp/scripts/dust-templates.js", "app/scripts/models/*.js","app/scripts/collections/*.js","app/scripts/views/*.js","app/scripts/routes/*.js", "app/scripts/main.js"],
+        dest: 'temp/scripts/include.js'
+      },
+      styles: {
+        src: ["app/styles/*.css"],
+        dest: 'temp/styles/main.css'
       }
     },
-
-    // specify an alternate install location for Bower
-    bower: {
-      dir: 'app/components'
-    },
-
-    // Coffee to JS compilation
-    coffee: {
-      compile: {
+    copy: {
+      dist:{
         files: {
-          'temp/scripts/*.js': 'app/scripts/**/*.coffee'
-        },
-        options: {
-          basePath: 'app/scripts'
+          'temp/': ['app/*']
         }
       }
     },
-
-    // compile .scss/.sass to .css using Compass
-    compass: {
-      dist: {
-        // http://compass-style.org/help/tutorials/configuration-reference/#configuration-properties
+    connect: {
+      server: {
         options: {
-          css_dir: 'temp/styles',
-          sass_dir: 'app/styles',
-          images_dir: 'app/images',
-          javascripts_dir: 'temp/scripts',
-          force: true
+          port:3000,
+          base: 'temp'
         }
       }
     },
-
-    // generate application cache manifest
-    manifest:{
-      dest: ''
-    },
-
-    // headless testing through PhantomJS
-    mocha: {
-      all: ['test/**/*.html']
-    },
-
-    server: {
-      // Personally I find open-browser annoying and so I remove it
-      app: 'clean lint compass coffee dustjs open-browser watch'
-    },
-
-    // default watch configuration
     watch: {
-      coffee: {
-        files: ['app/scripts/**/*.coffee','app/scripts/**/*.dust','app/scripts/**/*.js'],
-        tasks: 'coffee reload'
-      },
-      compass: {
-        files: [
-          'app/styles/**/*.{scss,sass}'
-        ],
-        tasks: 'compass reload'
-      },
       dustjs: {
         files: ['app/scripts/**/*.dust'],
-        tasks: 'coffee reload'
+        tasks: ['dustjs','concat']
       },
       concat: {
-        files: ['app/scripts/**/*.js'],
-        tasks: 'coffee reload'
+        files: ['app/scripts/**/*.js','app/styles/**/*.css'],
+        tasks: ['concat','copy']
       },
-      reload: {
-        files: [
-          'app/*.html',
-          'app/styles/**/*.css',
-          'app/scripts/**/*.js',
-          'app/scripts/**/*.dust',
-          'app/images/**/*'
-        ],
-        tasks: 'reload'
+      concat_main: {
+        files: ['app/*'],
+        tasks: 'copy'
       }
-    },
-
-    // default lint configuration, change this to match your setup:
-    // https://github.com/cowboy/grunt/blob/master/docs/task_lint.md#lint-built-in-task
-    lint: {
-      files: [
-        'Gruntfile.js',
-        'app/scripts/**/*.js',
-        'test/**/*.js'
-      ]
-    },
-
-    // specifying JSHint options and globals
-    // https://github.com/cowboy/grunt/blob/master/docs/task_lint.md#specifying-jshint-options-and-globals
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true
-      }
-    },
-
-    // Build configuration
-    // -------------------
-
-    // the staging directory used during the process
-    staging: 'temp',
-    // final build output
-    output: 'dist',
-
-    mkdirs: {
-      staging: 'app/'
-    },
-
-    // Below, all paths are relative to the staging directory, which is a copy
-    // of the app/ directory. Any .gitignore, .ignore and .buildignore file
-    // that might appear in the app/ tree are used to ignore these values
-    // during the copy process.
-
-    // concat css/**/*.css files, inline @import, output a single minified css
-    css: {
-      'styles/main.css': ['styles/**/*.css']
-    },
-
-    // renames JS/CSS to prepend a hash of their contents for easier
-    // versioning
-    rev: {
-      js: 'scripts/**/*.js',
-      css: 'styles/**/*.css',
-      img: 'images/**'
-    },
-
-    // usemin handler should point to the file containing
-    // the usemin blocks to be parsed
-    'usemin-handler': {
-      html: 'index.html'
-    },
-
-    // update references in HTML/CSS to revved files
-    usemin: {
-      html: ['**/*.html'],
-      css: ['**/*.css']
-    },
-
-    // HTML minification
-    html: {
-      files: ['**/*.html']
-    },
-
-    // Optimizes JPGs and PNGs (with jpegtran & optipng)
-    img: {
-      dist: '<config:rev.img>'
-    },
-
-    // rjs configuration. You don't necessarily need to specify the typical
-    // `path` configuration, the rjs task will parse these values from your
-    // main module, using http://requirejs.org/docs/optimization.html#mainConfigFile
-    //
-    // name / out / mainConfig file should be used. You can let it blank if
-    // you're using usemin-handler to parse rjs config from markup (default
-    // setup)
-    rjs: {
-      // no minification, is done by the min task
-      optimize: 'none',
-      baseUrl: './scripts',
-      wrap: true
     }
+
   });
 
-  // Alias the `test` task to run the `mocha` task instead
-  grunt.registerTask('test', 'mocha');
-
-  // Override build task to include dustjs
-  grunt.registerTask('coffee', ['dustjs', 'concat']);
-
-
-};
+  grunt.registerTask("default", ["clean","copy", "dustjs","concat"]);
+  grunt.registerTask("server", ["default","connect","watch"]);
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,35 +19,8 @@ Backbone.Model.prototype.toJSON = function() {
   return clone;
 };
 
-
-window.bins = {
-  Models: {},
-  Collections: {},
-  Views: {},
-  Routers: {},
-  init: function() {
-    console.log('Hello from Backbone!');
-    test();
-    console.log('test mode launched');
-  }
-};
-
 $(document).ready(function(){
   bins.init();
 });
 
 
-function test(){
-  var drawers = new bins.Collections.cabinetDrawers;
-  drawers.add(_.map(_.range(20),function(i){
-    return new bins.Models.Drawer;
-  }));
-  cab = new bins.Models.Cabinet({"drawers": drawers});
-
-  var cabV = new bins.Views.cabinetView({
-    el: $("#test"),
-    model: cab
-  });
-
-  cabV.render();
-}
